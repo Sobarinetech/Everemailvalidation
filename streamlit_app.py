25cbe59903e69464df4ece0d46b008440cd1bd1f
Sobarinetech <sobarinetechno@gmail.com>
2025-01-26 17:48:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,8 @@ st.title("Inboxify by EverTech")
 
 # Additional Information
 st.write("""
-**For Inboxify access, please visit [www.evertechnologies.in](http://www.evertechnologies.in)**  
-This version is created solely for **demo and testing purposes**.  
+**For Inboxify access, please visit [Amazon Appstore](https://www.amazon.com/gp/product/B0DV3S92JY)**  
+This version is created solely for **open source basis**.  
 The original application is capable of handling **50,000+ email IDs at once**.
 """)
 
